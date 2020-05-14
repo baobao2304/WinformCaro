3d67299d9d447a6fcb979a56755d682d8032a962
huutq <huutq@devblock.net>
2020-05-14 17:33:44 +0700

    Source code Caro Winform version 1

---
+++ PhamVanLeBao_2001170836_DevAppSmart_BT1/PhamVanLeBao_2001170836_DevAppSmart_BT1/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace PhamVanLeBao_2001170836_DevAppSmart_BT1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCapMT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCreateMatrix = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.player1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.player2 = new System.Windows.Forms.Label();
+            this.txttrai = new System.Windows.Forms.Label();
+            this.txtduoi = new System.Windows.Forms.Label();
+            this.txtphai = new System.Windows.Forms.Label();
+            this.txttren = new System.Windows.Forms.Label();
+            this.txtcheotraitren = new System.Windows.Forms.Label();
+            this.txtcheophaiduoi = new System.Windows.Forms.Label();
+            this.txtcheotraiduoi = new System.Windows.Forms.Label();
+            this.txtcheophaitren = new System.Windows.Forms.Label();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtCapMT
+            // 
+            this.txtCapMT.Location = new System.Drawing.Point(209, 12);
+            this.txtCapMT.Name = "txtCapMT";
+            this.txtCapMT.Size = new System.Drawing.Size(313, 22);
+            this.txtCapMT.TabIndex = 0;
+            this.txtCapMT.TextChanged += new System.EventHandler(this.txtCapMT_TextChanged);
+            this.txtCapMT.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCapMT_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Moi Nhap Cap Ma Tran";
+            // 
+            // btnCreateMatrix
+            // 
+            this.btnCreateMatrix.Location = new System.Drawing.Point(589, 13);
+            this.btnCreateMatrix.Name = "btnCreateMatrix";
+            this.btnCreateMatrix.Size = new System.Drawing.Size(119, 33);
+            this.btnCreateMatrix.TabIndex = 2;
+            this.btnCreateMatrix.Text = "Tao Ma Tran";
+            this.btnCreateMatrix.UseVisualStyleBackColor = true;
+            this.btnCreateMatrix.Click += new System.EventHandler(this.btnCreateMatrix_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.player1);
+            this.panel1.Location = new System.Drawing.Point(43, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(560, 27);
+            this.panel1.TabIndex = 3;
+            // 
+            // player1
+            // 
+            this.player1.AutoSize = true;
+            this.player1.ForeColor = System.Drawing.SystemColors.Control;
+            this.player1.Location = new System.Drawing.Point(212, 4);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(87, 17);
+            this.player1.TabIndex = 0;
+            this.player1.Text = "Nguoi choi 1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.player2);
+            this.panel2.Location = new System.Drawing.Point(43, 87);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(560, 27);
+            this.panel2.TabIndex = 4;
+            // 
+            // player2
+            // 
+            this.player2.AutoSize = true;
+            this.player2.ForeColor = System.Drawing.SystemColors.Control;
+            this.player2.Location = new System.Drawing.Point(212, 4);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(87, 17);
+            this.player2.TabIndex = 0;
+            this.player2.Text = "Nguoi choi 2";
+            // 
+            // txttrai
+            // 
+            this.txttrai.AutoSize = true;
+            this.txttrai.Location = new System.Drawing.Point(732, 58);
+            this.txttrai.Name = "txttrai";
+            this.txttrai.Size = new System.Drawing.Size(28, 17);
+            this.txttrai.TabIndex = 5;
+            this.txttrai.Text = "trai";
+            // 
+            // txtduoi
+            // 
+            this.txtduoi.AutoSize = true;
+            this.txtduoi.Location = new System.Drawing.Point(808, 97);
+            this.txtduoi.Name = "txtduoi";
+            this.txtduoi.Size = new System.Drawing.Size(46, 17);
+            this.txtduoi.TabIndex = 6;
+            this.txtduoi.Text = "label3";
+            // 
+            // txtphai
+            // 
+            this.txtphai.AutoSize = true;
+            this.txtphai.Location = new System.Drawing.Point(901, 54);
+            this.txtphai.Name = "txtphai";
+            this.txtphai.Size = new System.Drawing.Size(46, 17);
+            this.txtphai.TabIndex = 7;
+            this.txtphai.Text = "label4";
+            // 
+            // txttren
+            // 
+            this.txttren.AutoSize = true;
+            this.txttren.Location = new System.Drawing.Point(808, 21);
+            this.txttren.Name = "txttren";
+            this.txttren.Size = new System.Drawing.Size(46, 17);
+            this.txttren.TabIndex = 8;
+            this.txttren.Text = "label5";
+            // 
+            // txtcheotraitren
+            // 
+            this.txtcheotraitren.AutoSize = true;
+            this.txtcheotraitren.Location = new System.Drawing.Point(714, 9);
+            this.txtcheotraitren.Name = "txtcheotraitren";
+            this.txtcheotraitren.Size = new System.Drawing.Size(46, 17);
+            this.txtcheotraitren.TabIndex = 9;
+            this.txtcheotraitren.Text = "label4";
+            // 
+            // txtcheophaiduoi
+            // 
+            this.txtcheophaiduoi.AutoSize = true;
+            this.txtcheophaiduoi.Location = new System.Drawing.Point(901, 97);
+            this.txtcheophaiduoi.Name = "txtcheophaiduoi";
+            this.txtcheophaiduoi.Size = new System.Drawing.Size(46, 17);
+            this.txtcheophaiduoi.TabIndex = 10;
+            this.txtcheophaiduoi.Text = "label4";
+            // 
+            // txtcheotraiduoi
+            // 
+            this.txtcheotraiduoi.AutoSize = true;
+            this.txtcheotraiduoi.Location = new System.Drawing.Point(711, 97);
+            this.txtcheotraiduoi.Name = "txtcheotraiduoi";
+            this.txtcheotraiduoi.Size = new System.Drawing.Size(46, 17);
+            this.txtcheotraiduoi.TabIndex = 11;
+            this.txtcheotraiduoi.Text = "label4";
+            // 
+            // txtcheophaitren
+            // 
+            this.txtcheophaitren.AutoSize = true;
+            this.txtcheophaitren.Location = new System.Drawing.Point(901, 12);
+            this.txtcheophaitren.Name = "txtcheophaitren";
+            this.txtcheophaitren.Size = new System.Drawing.Size(46, 17);
+            this.txtcheophaitren.TabIndex = 12;
+            this.txtcheophaitren.Text = "label4";
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(1014, 13);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(119, 33);
+            this.btnUndo.TabIndex = 13;
+            this.btnUndo.Text = "Choi Lai";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1359, 905);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.txtcheophaitren);
+            this.Controls.Add(this.txtcheotraiduoi);
+            this.Controls.Add(this.txtcheophaiduoi);
+            this.Controls.Add(this.txtcheotraitren);
+            this.Controls.Add(this.txttren);
+            this.Controls.Add(this.txtphai);
+            this.Controls.Add(this.txtduoi);
+            this.Controls.Add(this.txttrai);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnCreateMatrix);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCapMT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCapMT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreateMatrix;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label player1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label player2;
+        private System.Windows.Forms.Label txttrai;
+        private System.Windows.Forms.Label txtduoi;
+        private System.Windows.Forms.Label txttren;
+        private System.Windows.Forms.Label txtphai;
+        private System.Windows.Forms.Label txtcheotraitren;
+        private System.Windows.Forms.Label txtcheophaiduoi;
+        private System.Windows.Forms.Label txtcheotraiduoi;
+        private System.Windows.Forms.Label txtcheophaitren;
+        private System.Windows.Forms.Button btnUndo;
+    }
+}
+
---
+++ PhamVanLeBao_2001170836_DevAppSmart_BT1/PhamVanLeBao_2001170836_DevAppSmart_BT1/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PhamVanLeBao_2001170836_DevAppSmart_BT1
+{
+    public partial class Form1 : Form
+    {
+        Panel pnCaro = new Panel();
+
+        private List<List<Button>> matrix;
+
+        public List<List<Button>> Matrix
+        {
+            get { return matrix; }
+            set { matrix = value; }
+        }
+
+        // isDisablePlayer use to do change player
+        bool isDisablePlayer = true;
+        public Form1()
+        {
+            InitializeComponent();
+            btnCreateMatrix.Enabled = false;
+        }
+
+        private void InitializeMatrixCaroComponent()
+        {
+
+
+
+        }
+
+
+        private void txtCapMT_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            this.Controls.Remove(pnCaro);
+          
+            // Xác thực rằng phím vừa nhấn không phải CTRL hoặc không phải dạng số
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // Nếu bạn muốn, bạn có thể cho phép nhập số thực với dấu chấm  , bo di la khong co dau cham
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        //btnCreateMatrix_Click  call matrix with textbox setup 
+        private void btnCreateMatrix_Click(object sender, EventArgs e)
+        {
+            //create matrix add button to do control win and lost
+            Matrix = new List<List<Button>>();
+            // set up panel caro
+            pnCaro.Size = new Size(1000, 400);
+            pnCaro.BackColor = Color.White;
+            pnCaro.Top = 150;
+            pnCaro.Left = 10;
+            pnCaro.AutoScroll = true;
+
+            int left = 3;
+            int top = 10;
+            this.Controls.Add(pnCaro);
+            for (int i = 0; i < int.Parse(this.txtCapMT.Text.ToString()); i++)
+            {
+                Matrix.Add(new List<Button>());
+                for (int j = 0; j < int.Parse(this.txtCapMT.Text.ToString()); j++)
+                {
+                    Button btnPoint = new Button();
+                    btnPoint.Size = new Size(30, 30);
+                    btnPoint.Left = left + 33;
+                    btnPoint.Top = top;
+                    left += 33;
+                    btnPoint.BackColor = Color.Gray;
+                    btnPoint.Text = "";
+                    //set up vertical for button;
+                    btnPoint.Tag = i.ToString();
+                    this.pnCaro.Controls.Add(btnPoint);
+                    //set up handler click event
+                    btnPoint.Click += btn_Click;
+                    //add matrix to do save location of button handle win and lost
+                    Matrix[i].Add(btnPoint);
+                     
+                }
+                top += 30;
+                left = 3;
+            }
+
+
+        }
+
+        private void txtCapMT_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCapMT.Text == "")
+            {
+                txtCapMT.Text = "0";
+            }
+            int txtCapMTparse = int.Parse(txtCapMT.Text.ToString());
+            if (txtCapMTparse > 6)
+                btnCreateMatrix.Enabled = true;
+            else
+                btnCreateMatrix.Enabled = false;
+            if (txtCapMTparse > 50)
+                btnCreateMatrix.Enabled = false;
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            
+            ChangePlayer(btn);
+            HandleWinandLost(btn);
+        }
+        private void ChangePlayer(Button btn)
+        {
+            if (this.isDisablePlayer)
+            {
+                btn.Text = "X";
+                isDisablePlayer = false;
+            }
+            else
+            {
+                btn.Text = "O";
+                isDisablePlayer = true;
+            }
+            btn.Enabled = false;
+        }
+        private void HandleWinandLost(Button btn)
+        {
+            //call vertical location of  button click   hang doc X
+            int vertical = Convert.ToInt32(btn.Tag);
+            int vertical2 = vertical;
+            //call horizontal location of  button click hang ngang Y
+            int horizontal = Matrix[vertical].IndexOf(btn);
+            int horizontal2 = horizontal;
+            int tren = 0, duoi = 0, trai = 0, phai = 0, cheotraitren = 0, cheophaitren = 0, dem = 1, cheotraiduoi = 0,cheophaiduoi = 0;
+
+        
+            // hander win from vertical
+            for (int i = horizontal; i > 0; i--)
+                {
+                    // check X from better to left
+                    if (Matrix[vertical][i].Text == btn.Text.ToString())
+                    {
+                        trai++;
+                    }
+                    else
+                        break;
+                    
+                }
+
+            for (int i = horizontal; i < int.Parse(txtCapMT.Text.ToString()); i++)
+                {
+                    // check X from better to right
+                    if (Matrix[vertical][i].Text == btn.Text.ToString())
+                    {   
+                        phai++;
+                    }
+                    else
+                        break;
+                }
+            if (trai + phai >= 6)
+                MessageBox.Show("Player " + btn.Text.ToString() + " win");
+            // hander win from horizontal
+            for (int i = vertical; i > 0; i--)
+            {
+                // check X from better to top
+                if (Matrix[i][horizontal].Text == btn.Text.ToString())
+                {
+                    tren++;
+                }
+                else
+                    break;
+
+            }
+
+            for (int i = vertical; i < int.Parse(txtCapMT.Text.ToString()); i++)
+            {
+                // check X from better to bottom
+                if (Matrix[i][horizontal].Text == btn.Text.ToString())
+                {
+                    duoi++;
+                }
+                else
+                    break;
+
+            }
+            if (tren + duoi >= 6)
+            {
+                MessageBox.Show("Player " + btn.Text.ToString() + " win");
+                tren = duoi = 0;
+            }
+                
+            // hander win from cheotrai
+
+            for (int i = 0; i <= vertical; i++)
+            {
+                if (vertical - i < 0 || horizontal - i < 0)
+                    break;
+                if (Matrix[horizontal - i][vertical - i].Text == btn.Text.ToString())
+                {
+                    cheotraitren++;
+                }
+                else
+                    break;
+            }
+
+            for (int i = 1; i <= int.Parse(txtCapMT.Text.ToString()) - vertical; i++)
+            {
+                if (horizontal + i >= int.Parse(txtCapMT.Text.ToString()) || vertical + i >= int.Parse(txtCapMT.Text.ToString()))
+                    break;
+
+                if (Matrix[horizontal + i][vertical + i].Text == btn.Text.ToString())
+                {
+                    cheophaiduoi++;
+                }
+                else
+                    break;
+            }
+
+            if(cheophaiduoi + cheotraitren == 5)
+            {
+                MessageBox.Show("Player " + btn.Text.ToString() + " win");
+                cheophaiduoi = cheotraitren = 0;
+            }
+
+
+          
+
+            for (int i = 0; i <= vertical; i++)
+            {
+                if (vertical + i > int.Parse(txtCapMT.Text.ToString()) || horizontal - i < 0)
+                    break;
+
+                if (Matrix[horizontal - i][vertical + i].Text == btn.Text)
+                {
+                    cheophaitren++;
+                }
+                else
+                    break;
+            }
+
+            for (int i = 1; i <= int.Parse(txtCapMT.Text.ToString()) - vertical; i++)
+            {
+                if (horizontal + i >= int.Parse(txtCapMT.Text.ToString()) || vertical - i < 0)
+                    break;
+
+                if (Matrix[horizontal + i][vertical - i].Text == btn.Text)
+                {
+                    cheotraiduoi++;
+                }
+                else
+                    break;
+            }
+
+            if (cheophaitren + cheotraiduoi >= 5)
+            {
+                MessageBox.Show("Player " + btn.Text.ToString() + " win");
+                cheophaitren = cheotraiduoi = 0;
+            }
+
+            //player1.Text = vertical.ToString();
+            //player2.Text = horizontal.ToString();
+
+            txttren.Text = "tren :" + tren.ToString();
+            txtduoi.Text = "duoi :" + duoi.ToString();
+            txtphai.Text = "phai :"+phai.ToString();
+            txttrai.Text = "trai :"+trai.ToString();
+
+            txtcheotraitren.Text = "ctt :" + cheotraitren.ToString();
+            txtcheotraiduoi.Text = "ctd :" + cheotraiduoi.ToString();
+            txtcheophaitren.Text = "cpt :" + cheophaitren.ToString();
+            txtcheophaiduoi.Text = "cpd :" + cheophaiduoi.ToString();
+            tren = 0;
+            duoi = 0;
+            trai = 0;
+            phai = 0;
+            cheotraitren = 0;
+            cheophaitren = 0;
+            dem = 1;
+            cheotraiduoi = 0;
+            cheophaiduoi = 0;
+
+
+        }
+    }
+}
